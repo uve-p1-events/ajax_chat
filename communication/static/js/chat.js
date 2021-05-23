4a18d6a90ceddb33336ce3018bdb9923665b33a7
abhyamgupta123 <abhyamgupta123@gmail.com>
2021-05-24 01:14:26 +0530

    added moderation method

--- communication/static/js/chat.js
+++ communication/static/js/chat.js
@@ -1,4 +1,10 @@
-// alert('{{ STATIC_URL|escapejs }}');
+// initialising all the local variables
+var user_typing_status = false;
+var startingtime = new Date();
+var latestChat;
+var oldestChat;
+
+// function to send the user input message to server with passing all other required variables and parameters by "POST" request
 function sendMessage(){
     text=$('#msg').val();
     if (text.length>0) {
@@ -12,7 +18,8 @@ function sendMessage(){
                 recipient: reader, 
                 groupFlag: onGroup, 
                 group_id: groupId, 
-                currentStamp: currentTimestamp, 
+                currentStamp: currentTimestamp,
+                group_protection: groupProtection,  
                 csrfmiddlewaretoken: crsfTocken
             }
         })
@@ -26,37 +33,68 @@ function sendMessage(){
     }
 }
 
-// enter key listener
+
+// enter key listener. Send the message typed in message box to server if enter key is pressed by calling sendMessage function.
 window.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
         sendMessage();
     }
 }, false);
 
+// On click listener for Send button
 $('#btn').click(function(event) {
     /* Act on the event */
     sendMessage();
 });
 
-// $.ajax({
-//         url: getChatsUrl,
-//         type: 'POST',
-//         dataType: 'json',
-//         data: {typingStatus: false, recipient: reader, groupFlag: onGroup, group_id: groupId, currentStamp: currentTimestamp, csrfmiddlewaretoken: crsfTocken}
-//     })
-//     .done(function(data) {
-//         console.log("mai hu sperman");
-//         console.log(data);
-//         console.log(data.chats);
-//     })
-//     .fail(function(error) {
-//         console.log("error is 1", error);
-//     })
+// On click listener for load message button.
+$("#loadMessage").click(function(event){
+    var oldChatTempDict = {
+                            group_id: oldestChat.groupId,
+                            id: oldestChat.id,
+                            is_group: oldestChat.isGroup,
+                            owner: oldestChat.owner,
+                            recipient: oldestChat.recipient,
+                            text: oldestChat.text,
+                            normal_timestamp: oldestChat.normalTimestamp
+                          };
+    $.ajax({
+        url: loadmessagesurl,
+        type: 'POST',
+        dataType: 'json',
+        data:{
+                dataObject: JSON.stringify(oldChatTempDict),
+                recipient: reader, 
+                groupFlag: onGroup, 
+                group_id: groupId, 
+                csrfmiddlewaretoken: crsfTocken
+            }
+        })
+        .done(function(data) {
+            // console.log("data is ", data);
+            if (data.chats.length == 0){
+                window.alert("No more messages to load");
+            }
+            for(let i = data.chats.length-1; i>=0; i--){
+                $('.container').prepend(`<img src="/w3images/bandmember.jpg" alt="${data.chats[i].owner}" class="deltableContainer">
+                                         <p class="deltableContainer">${data.chats[i].text}</p>
+                                         <span class="time-right deltableContainer">${data.chats[i].timestamp}</span>`);
+            }
 
-var user_typing_status = false;
+            // initialising the oldest chat variable with the oldest message object recieved when the server sends the list of 20 messages after requesting.
+            if (data.chats.length != 0){
+                oldestChat = data.chats[0];
+            }
+        })
+        .fail(function() {
+            console.log("error");
+        })
+    console.log(oldestChat);
+});
+
+
+// setting on typing listener on message box and initiallising the required variables to update the user status as typing or not
 const input = document.querySelector('#msg');
-
-var startingtime = new Date();
 input.addEventListener("input", function(s) {
     console.log("user is typing");
     user_typing_status = true;
@@ -64,10 +102,6 @@ input.addEventListener("input", function(s) {
 });
 
 // window.document.querySelector('.typing_status').innerHTML = 'abhyam';
-
-var latestChat;
-console.log("before updatetion value of latestchat is ", latestChat);
-// console.log("its length is ", latestChat.length);
 
 window.setInterval(() => {
     // detecting if user is not typing for more than 5 seconds.
@@ -95,14 +129,15 @@ window.setInterval(() => {
         // $("span").remove(".deltableContainer");
             console.log("executed from length >0");
             console.log("from >0 value of latestchat is", latestChat);
+            
             let chatIndex = data.chats.findIndex((element) => {
-                if(element.id = latestChat.id && element.owner == latestChat.owner && element.groupId == latestChat.groupId && element.text == latestChat.text && element.isgroup == latestChat.isgroup){
+                if(element.id == latestChat.id && element.owner == latestChat.owner && element.groupId == latestChat.groupId && element.text == latestChat.text && element.isgroup == latestChat.isgroup){
                     return true;
-                }
-                else{
+                }else{
                     return false;
                 }
             });
+
             console.log("last index is", chatIndex);
             console.log("chats list is =>", data.chats);
             console.log("chats list length is =>", data.chats.length);
@@ -110,36 +145,25 @@ window.setInterval(() => {
                 $('.container').append(`<img src="/w3images/bandmember.jpg" alt="${data.chats[i].owner}" class="deltableContainer">
                                         <p class="deltableContainer">${data.chats[i].text}</p>
                                         <span class="time-right deltableContainer">${data.chats[i].timestamp}</span>`);
-                if (i == (data.chats.length -1)){
-                    // if (latestChat.length>0){
-                    //     latestChat.pop();
-                    // }
-                    latestChat = data.chats[i];
-                }
             }
+
+            if(data.chats.length != 0){
+                latestChat = data.chats[data.chats.length -1];
+            }
+
         }else{
             console.log("executed from length =0");
             for(let i = 0; i<data.chats.length; i++){
                 $('.container').append(`<img src="/w3images/bandmember.jpg" alt="${data.chats[i].owner}" class="deltableContainer">
                                         <p class="deltableContainer">${data.chats[i].text}</p>
                                         <span class="time-right deltableContainer">${data.chats[i].timestamp}</span>`);
-                if (i == (data.chats.length -1)){
-                    // if (latestChat.length>0){
-                    //     latestChat.pop();
-                    // }
-                    latestChat = data.chats[i];
-                }
+            }
+
+            if(data.chats.length != 0){
+                oldestChat = data.chats[0];
+                latestChat = data.chats[data.chats.length -1];
             }
         }
-        // for(let i = 0;i<data.chats.length;i++){
-        //     $('.container').append(`<img src="/w3images/bandmember.jpg" alt="${data.chats[i].owner}" class="deltableContainer">
-        //                             <p class="deltableContainer">${data.chats[i].text}</p>
-        //                             <span class="time-right deltableContainer">${data.chats[i].timestamp}</span>`);
-        //     if (i == (data.chats.length -1) && latestChat.length>0){
-        //         latestChat.pop();
-        //         latestChat.push(data.chats[i]);
-        //     }
-        // }
 
         if(data.group_flag == true){
             var s = "";
@@ -155,8 +179,7 @@ window.setInterval(() => {
 
             if (s != ""){
                 window.document.querySelector('.typing_status').innerHTML = `users typing are => ${s}`;
-            }
-            else{
+            }else{
                 window.document.querySelector('.typing_status').innerHTML = s;
             }
             // console.log(`users typing are => ${data.typing_status[0].owner}`);
